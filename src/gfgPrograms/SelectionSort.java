df3a006f535246cffe1c7b0f4875aaf46a472634
Aastha-520609 <aasthachaudhary5050@gmail.com>
2022-10-08 00:26:16 +0530

    selection Sorting

---
+++ src/gfgPrograms/SelectionSort.java
@@ -0,0 +1,36 @@
+package gfgPrograms;
+import java.util.*;
+public class SelectionSort {
+	public static void selectionSort(int arr[], int n)
+	{
+	    //code here
+	    for(int i=0; i<n-1; i++)
+	    {
+	        int min = i;
+	        for(int j=i+1; j<n; j++)
+	        {
+	            if(arr[j] < arr[min])
+	            {
+	                min = j;
+	            }
+	        }
+	        if(min != i)
+	        {
+	            int temp = arr[min];
+	            arr[min] = arr[i];
+	            arr[i] = temp;
+	        }
+	    }
+	}
+	public static void main(String[] args)
+	   {
+		int arr[] = {4,7,8,89,1};
+		int n = 5;
+		selectionSort(arr,n);
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+	   }
+
+}

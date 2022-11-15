d85b0ef01aaf5ad689e4b041206f73055a90c2e3
Aastha-520609 <aasthachaudhary5050@gmail.com>
2022-11-15 23:58:25 +0530

    revising

--- src/peepcodingPrograms/NextGreaterElement.java
+++ src/peepcodingPrograms/NextGreaterElement.java
@@ -34,16 +34,22 @@ public class NextGreaterElement {
 		Stack<Integer> st = new Stack <> ();
 		st.push(arr[arr.length - 1]);
 		nge[arr.length - 1] = -1;
+		//1. The element is pop out of the stack if it is smaller than the upcoming element.
+		//2. If no element is smaller in the stack than than the upcoming element then the element present in the stack is its greatest element 
+		// and the upcoming element is pushed into stack.
 		for(int i=arr.length-2; i>=0; i--)
 		{
+			//the element is pop if stack has some element in it and if the coming element is greater and equal to the peek element
 			while(st.size() > 0 && arr[i] >= st.peek())
 			{
 				st.pop();
 			}
+			//if the stack has no element than the coming element has no greater element so -1.
 			if(st.size() == 0)
 			{
 				nge[i] = -1;
 			}
+			//put the coming element to the peek of the stack after checking
 			else
 			{
 				nge[i] = st.peek();

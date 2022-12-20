d82f9ce68c0b700720455a0862e2f9c99d50fdce
Aastha-520609 <aasthachaudhary5050@gmail.com>
2022-12-20 18:06:16 +0530

    done using stack

--- src/gfgPrograms/immediateSmallerElem.java
+++ src/gfgPrograms/immediateSmallerElem.java
@@ -2,7 +2,7 @@ package gfgPrograms;
 import java.util.*;
 public class immediateSmallerElem {
 	public static void immediateSmaller(int arr[], int n) {
-        for(int i=0; i<n; i++)
+       /* for(int i=0; i<n; i++)
         {
             if(i+1 < n && arr[i+1] < arr[i])
             {
@@ -11,6 +11,22 @@ public class immediateSmallerElem {
             else
             {
                 arr[i] = -1;
+            }
+        }*/
+		Stack<Integer> stack= new Stack<>();
+        stack.push(-1);
+        for(int i=n-1; i>=0; i--)
+        {
+            int ele = arr[i];
+            if(stack.peek() < ele)
+            {
+                arr[i] = stack.peek();
+                stack.push(ele);
+            }
+            else
+            {
+                arr[i] = -1;
+                stack.push(ele);
             }
         }
     }

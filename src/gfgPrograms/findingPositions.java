1d230e70483013a5a246b0354b1debe9130206e6
Aastha-520609 <aasthachaudhary5050@gmail.com>
2023-03-23 23:40:13 +0530

    added question to the file

--- src/gfgPrograms/findingPositions.java
+++ src/gfgPrograms/findingPositions.java
@@ -1,6 +1,13 @@
 package gfgPrograms;
 import java.io.*;
 import java.util.*;
+/* Question:
+Some people(n) are standing in a queue. A selection process follows a rule where people standing on even positions are selected. Of the selected people 
+a queue is formed and again out of these only people on even position are selected. This continues until we are left with one person. 
+Find out the position of that person in the original queue.
+Sample Input : n = 5;
+1 2 3 4 5 - number at even places is 2 4 and again number in even is 4. 
+*/
 public class findingPositions {
 	 public static long nthPosition(long n){
 	        return findPosition(1,n);

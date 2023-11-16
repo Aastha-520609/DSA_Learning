c5c5d8c1084551ac2be0437a76fb365f6935b90c
Aastha-520609 <77205408+Aastha-520609@users.noreply.github.com>
2023-11-16 21:58:17 +0530

    making all the cols and rows zero of the particular cell

---
+++ src/revision/SetZeros.java
@@ -0,0 +1,100 @@
+package revision;
+import java.util.*;
+import java.io.*;
+
+public class SetZeros {
+	public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if the first row should be set to zero
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column should be set to zero
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and first column to mark zeros in the rest of the matrix
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set the marked rows to zero
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < cols; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set the marked columns to zero
+        for (int j = 1; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < rows; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set the first row to zero if needed
+        if (firstRowZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set the first column to zero if needed
+        if (firstColZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        SetZeros setZeros = new SetZeros();
+
+        // Example matrix, replace it with your matrix
+        int[][] matrix = {
+            {1, 1, 1, 1},
+            {1, 0, 0, 1},
+            {1, 0, 0, 1},
+            {0, 1, 1, 1}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        setZeros.setZeroes(matrix);
+
+        System.out.println("Resultant Matrix:");
+        printMatrix(matrix);
+    }
+}

fa622b622fc14ad0316163b45515c210d5e0160d
Aastha-520609 <77205408+Aastha-520609@users.noreply.github.com>
2024-03-12 10:55:44 +0530

    Generating parenthesis using dp

---
+++ src/revision/GenerateParenthesis.java
@@ -0,0 +1,39 @@
+package revision;
+import java.util.*;
+public class GenerateParenthesis {
+	public static List<String> generate(int n) {
+        List<String>[] dp = new List[n+1];
+        dp[0] = Arrays.asList("");
+        dp[1] = Arrays.asList("()");
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = new ArrayList<>();
+            int inner = i-1;
+            int outer = 0;
+            while(inner >= 0 && outer<= i-1)
+            {
+              for(String in: dp[inner])
+              {
+                for(String out: dp[outer])
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("(");
+                    sb.append(in);
+                    sb.append(")");
+                    sb.append(out);
+                    dp[i].add(sb.toString());
+                }
+              }
+              inner --;
+              outer ++;
+            }
+        }
+        return dp[n];
+    }
+	public static void main(String args[])
+	{
+		 int n = 3; // Change n to the desired value
+	     List<String> result = generate(n);
+	     System.out.println("Valid combinations of parentheses pairs for n = " + n + ": " + result);
+	}
+}

11df7c4d5f798caedb3d514c8f6f5d19d3ce6775
Aastha-520609 <77205408+Aastha-520609@users.noreply.github.com>
2024-07-25 00:33:51 +0530

    finding maximum profit by buying and selling goods

---
+++ src/revision/SellAndBuy_I.java
@@ -0,0 +1,19 @@
+package revision;
+import java.io.*;
+public class SellAndBuy_I {
+	 public static int maxProfit(int[] prices) {
+	        int mini = prices[0];
+	        int maxProfit = 0;
+	        for(int i=1; i<prices.length; i++){
+	            int cost = prices[i] - mini;
+	            maxProfit = Math.max(maxProfit, cost);
+	            mini = Math.min(prices[i], mini);
+	        }
+	        return maxProfit;
+	    }
+	 public static void main(String args[]) {
+		 
+		 int[] prices = {7,1,5,3,6,4};
+		 System.out.println(maxProfit(prices));
+	 }
+}

ffac67919aa8a3dc6b87000f49f4451d85a9eac5
Aastha-520609 <77205408+Aastha-520609@users.noreply.github.com>
2024-08-01 15:33:35 +0530

    doing string revision

---
+++ src/revision/Revision.java
@@ -0,0 +1,21 @@
+package revision;
+import java.util.*;
+public class Revision {
+	public static void reverseString(char[] s) {
+	       int left = 0;
+	       int right = s.length - 1;
+	       while(left < right){
+	        char temp = s[left];
+	        s[left] = s[right];
+	        s[right] = temp;
+
+	        left++;
+	        right--;
+	       }
+	    }
+	public static void main(String args[]) {
+		char[] str = {'h', 'e', 'l','l', 'o'};
+		reverseString(str);
+		System.out.println(str);
+	}
+}

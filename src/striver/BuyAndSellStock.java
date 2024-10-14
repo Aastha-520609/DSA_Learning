8fb26f2bea58c126e0d5829cc917d1fe5751b952
Aastha-520609 <77205408+Aastha-520609@users.noreply.github.com>
2024-10-14 21:41:57 +0530

    buy and sell stock 2

--- src/striver/BuyAndSellStock.java
+++ src/striver/BuyAndSellStock.java
@@ -14,8 +14,8 @@ public class BuyAndSellStock {
 		return maxProfit;
 	}
 	public static void main(String args[]) {
-		//int[] prices = {7,1,5,3,6,4};
-		int[] prices = {7,6,4,3,1};
+		int[] prices = {7,1,5,3,6,4};
+		//int[] prices = {7,6,4,3,1};
 		System.out.println(profit(prices));
 	}
 
